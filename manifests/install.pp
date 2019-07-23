3526820fd589cee61ddccb7bddab373080697548
root <root@carrb1c.mylabserver.com>
2019-07-23 14:43:29 +0000

    Added debian support with my_params.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,11 @@
 # @summary 
 #     Installs apache
 #
-class apache::install {
-  package { 'https':
-    ensure => present,
+class apache::install (
+  $install_name   = $apache::my_params::install_name,
+  $install_ensure = $apache::my_params::install_ensure, 
+) inherits appache::my_params {
+  package { "${install_name}":
+    ensure => $install_ensure,
   }
 }
