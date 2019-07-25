c7163a6f74238ec251f56f9345cfb7cd4597b59d
root <root@carrb1c.mylabserver.com>
2019-07-25 07:54:49 +0000

    Added config class to mage config files

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,8 @@
 # @summary 
 #     Installs apache
 #
-class apache::install (
-  $install_name   = $apache::my_params::install_name,
-  $install_ensure = $apache::my_params::install_ensure, 
-) inherits appache::my_params {
-  package { "${install_name}":
-    ensure => $install_ensure,
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
